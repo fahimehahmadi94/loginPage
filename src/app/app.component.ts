fb38e83c20729b01ed1d245b7c0699426044c81e
fahimehahmadi.fafa <fahimehahmadi94@gmail.com>
2022-08-24 22:27:47 +0430

    Add UI Login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'loginPage';
+export class AppComponent implements OnInit {
+
+  textDirection: string = '';
+
+  constructor(public translate: TranslateService) {
+    this.CheckDirection();
+    // Register translation languages
+    translate.addLangs(['English', 'Persian']);
+
+    // Set default language
+    translate.setDefaultLang('English');
+
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('language') == 'English') {
+      console.log("testtttttttttt");
+
+    }
+
+    if (localStorage.getItem('language') == null) {
+      localStorage.setItem('language', 'English');
+    }
+    else {
+
+      let lang = localStorage.getItem('language');
+      this.translate.setDefaultLang(`${lang}`);
+      this.translateLanguageTo(`${lang}`);
+    }
+
+
+
+  }
+
+  CheckDirection() {
+    const lang = localStorage.getItem('language');
+    if (lang == 'Persian') {
+      this.textDirection = 'rtl'
+    }
+    else {
+      this.textDirection = 'ltr'
+
+    }
+  }
+
+  //Switch language
+  translateLanguageTo(language: string) {
+    localStorage.setItem('language', language);
+    language = language;
+
+    this.translate.use(language);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function httpTranslateLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
